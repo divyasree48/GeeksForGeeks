517f2b0213ec0e3dc33705d98296ff4bd13d3817
Divya <divyasreem48@gmail.com>
2024-01-09 17:43:03 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Factorials of large numbers/factorials-of-large-numbers.py
@@ -0,0 +1,31 @@
+#User function Template for python3
+
+class Solution:
+    def factorial(self, n):
+        c=1
+        for i in range(2,n+1):
+            c*=i
+        l=[]
+        while(c):
+            l.append(c%10)
+            c//=10
+        return l[::-1]
+                
+            
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t=int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ans = ob.factorial(N);
+        for i in ans:
+            print(i,end="")
+        print()
+    
+# } Driver Code Ends
