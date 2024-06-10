7e005bf0c362bc09a7d11e847c7f436c876cf16b
Divya Sree M <divyasreem48@gmail.com>
2024-06-10 20:13:22 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Nuts and Bolts Problem/nuts-and-bolts-problem.py
@@ -0,0 +1,36 @@
+#User function Template for python3
+class Solution:
+
+	def matchPairs(self, n, nuts, bolts):
+		# code here
+		s='!#$%&*?@^'
+		l=[]
+		for i in s:
+		    if i in nuts:
+		        l.append(i)
+		for i in range(n):
+		    nuts[i]=l[i]
+		    bolts[i]=l[i]
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n = int(input())
+        nuts = list(map(str, input().strip().split()))
+        bolts = list(map(str, input().strip().split()))
+        ob = Solution()
+        ob.matchPairs(n, nuts, bolts)
+        for x in nuts:
+            print(x, end=" ")
+        print()
+        for x in bolts:
+            print(x, end=" ")
+        print()
+        tc -= 1
+
+# } Driver Code Ends
